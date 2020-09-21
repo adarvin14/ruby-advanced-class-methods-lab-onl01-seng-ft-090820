33220a8c987ec72f748f0e0e493561a97a458d6e
Alex Darvin <amdarvin14@gmail.com>
2020-09-21 21:29:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,16 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+ 
   def self.all
     @@all
   end
-
+ 
   def save
     self.class.all << self
   end
-
+  def new_by_name(name)
+    @name = name
+  end
+ 
 end
